ef517debe59bca7d3e170a65c027a3c4851039cc
wendy hsiao <wendy19940331@gmail.com>
2019-10-30 00:47:50 +0800

    Initialize sequelize and connect to database

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const handlebars = require('express-handlebars')
+const db = require('./models') // 引入資料庫
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 
 
 app.listen(port, () => {
+  db.sequelize.sync() // 跟資料庫同步
   console.log(`Example app listening on port ${port}!`)
 })
 
