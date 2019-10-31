3710126d8bd7fdcca623e8fd64ca8b64b75afe2f
wendy hsiao <wendy19940331@gmail.com>
2019-11-01 02:13:02 +0800

    Install passport and passport-local, add feature: signInPage, signIn and logout

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const db = require('./models') // 引入資料庫
+
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'your secrete key', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -24,4 +28,4 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
