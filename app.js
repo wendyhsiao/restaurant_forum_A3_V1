0dd7bcea7942e62f8cbb1a1097caf3c2f3f04e9d
wendy hsiao <wendy19940331@gmail.com>
2019-11-01 00:48:35 +0800

    Install express-session connect-flash, confirm user password and unique email

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const app = express()
 const port = 3000
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models') // 引入資料庫
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'your secrete key', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync() // 跟資料庫同步
