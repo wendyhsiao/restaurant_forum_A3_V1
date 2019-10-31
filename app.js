8277443dc2db991d413ac5f86cd8e2afca9f7a22
wendy hsiao <wendy19940331@gmail.com>
2019-11-01 00:13:46 +0800

    Install bcrypt-nodejs, body-parser, add userController and signup page

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const db = require('./models') // 引入資料庫
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   db.sequelize.sync() // 跟資料庫同步
