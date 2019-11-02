7cb79ff3680015c2f1e1a8512dbc3e9a4a4e564d
wendy hsiao <wendy19940331@gmail.com>
2019-11-02 15:25:32 +0800

    Listen to process.env.PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
