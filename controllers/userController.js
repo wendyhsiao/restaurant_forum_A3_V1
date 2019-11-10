3b43c0fac99fa323469c943bb91599329ae30801
wendy hsiao <wendy19940331@gmail.com>
2019-11-10 10:54:50 +0800

    Add addFollowing and removeFollowing to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,7 @@ const Comment = db.Comment
 const Restaurant = db.Restaurant
 const Favorite = db.Favorite
 const Like = db.Like
+const Followship = db.Followship
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
@@ -147,6 +148,29 @@ const userController = {
       return res.render('topUser', { users: users })
 
     })
+  },
+  addFollowing: (req, res) => {
+    return Followship.create({
+      followerId: req.user.id,
+      followingId: req.params.userId
+    })
+      .then((followship) => {
+        return res.redirect('back')
+      })
+  },
+  removeFollowing: (req, res) => {
+    return Followship.findOne({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId
+      }
+    })
+      .then((followship) => {
+        followship.destroy()
+          .then((followship) => {
+            return res.redirect('back')
+          })
+      })
   }
 }
 
