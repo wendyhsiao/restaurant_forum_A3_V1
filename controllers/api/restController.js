727c56ef17809c7bd828c106666c0f9bec26650d
wendy hsiao <wendy19940331@gmail.com>
2019-11-17 02:42:08 +0800

    Refactor restController, restService

--- controllers/api/restController.js
+++ controllers/api/restController.js
@@ -5,32 +5,32 @@ const Comment = db.Comment
 const User = db.User
 const pageLimit = 10
 
-const restService = require('../services/restService.js')
+const restService = require('../../services/restService.js')
 
 const restController = {
   getRestaurants: (req, res) => {
     restService.getRestaurants(req, res, data => {
-      return res.render('restaurants', data)
+      return res.json(data)
     })
   },
   getRestaurant: (req, res) => {
     restService.getRestaurant(req, res, data => {
-      return res.render('restaurant', data)
+      return res.json(data)
     })
   },
   getDashboard: (req, res) => {
     restService.getDashboard(req, res, data => {
-      return res.render('dashboard', data)
+      return res.json(data)
     })
   },
   getFeeds: (req, res) => {
     restService.getFeeds(req, res, data => {
-      return res.render('feeds', data)
+      return res.json(data)
     })
   },
   getTopRestaurant: (req, res) => {
     restService.getTopRestaurant(req, res, data => {
-      return res.render('topRestaurant', data)
+      return res.json(data)
     })
   }
 }
